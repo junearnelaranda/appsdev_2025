3d291120ad1fc7f0587310af495827b47867ea62
June Arnel Aranda <142424794+junearnelaranda@users.noreply.github.com>
2025-05-17 16:56:16 +0800

    Add files via upload

---
+++ Chapter 1/EyeWear/BrowlineForm - Copy (2).cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EyeWear
+{
+    public partial class BrowlineForm : Form
+    {
+        private string _username;
+        public BrowlineForm(string userName)
+        {
+            InitializeComponent();
+            _username = userName;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://ph.sunniesstudios.com/collections/frames-for-round-faces/products/wes",
+                UseShellExecute = true
+            });
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://ph.sunniesstudios.com/collections/frames-for-round-faces/products/madison",
+                UseShellExecute = true
+            });
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://ph.sunniesstudios.com/collections/frames-for-round-faces/products/clyde",
+                UseShellExecute = true
+            });
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://ph.sunniesstudios.com/collections/frames-for-round-faces/products/bastian",
+                UseShellExecute = true
+            });
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://ph.sunniesstudios.com/collections/frames-for-round-faces/products/gregor",
+                UseShellExecute = true
+            });
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://ph.sunniesstudios.com/collections/frames-for-round-faces/products/ollie",
+                UseShellExecute = true
+            });
+        }
+
+        private void BrowlineBack_Click(object sender, EventArgs e)
+        {
+            RoundForm roundForm = new RoundForm(_username);
+            this.Hide();
+            roundForm.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ Chapter 1/EyeWear/BrowlineForm.Designer - Copy - Copy.cs
@@ -0,0 +1,451 @@
+﻿namespace EyeWear
+{
+    partial class BrowlineForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BrowlineForm));
+            panel8 = new Panel();
+            panel9 = new Panel();
+            label7 = new Label();
+            button7 = new Button();
+            pictureBox7 = new PictureBox();
+            panel10 = new Panel();
+            label8 = new Label();
+            button8 = new Button();
+            pictureBox8 = new PictureBox();
+            panel11 = new Panel();
+            label9 = new Label();
+            button9 = new Button();
+            pictureBox9 = new PictureBox();
+            panel12 = new Panel();
+            label10 = new Label();
+            button10 = new Button();
+            pictureBox10 = new PictureBox();
+            panel13 = new Panel();
+            pictureBox6 = new PictureBox();
+            label11 = new Label();
+            button11 = new Button();
+            panel14 = new Panel();
+            label12 = new Label();
+            button12 = new Button();
+            pictureBox12 = new PictureBox();
+            BrowlineBack = new Button();
+            panel8.SuspendLayout();
+            panel9.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            panel10.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            panel11.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            panel12.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            panel13.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            panel14.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
+            SuspendLayout();
+            // 
+            // panel8
+            // 
+            panel8.BackColor = Color.White;
+            panel8.BorderStyle = BorderStyle.FixedSingle;
+            panel8.Controls.Add(panel9);
+            panel8.Controls.Add(panel10);
+            panel8.Controls.Add(panel11);
+            panel8.Controls.Add(panel12);
+            panel8.Controls.Add(panel13);
+            panel8.Controls.Add(panel14);
+            panel8.Location = new Point(119, 103);
+            panel8.Margin = new Padding(3, 4, 3, 4);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(1213, 781);
+            panel8.TabIndex = 17;
+            // 
+            // panel9
+            // 
+            panel9.BackColor = Color.Silver;
+            panel9.Controls.Add(label7);
+            panel9.Controls.Add(button7);
+            panel9.Controls.Add(pictureBox7);
+            panel9.Location = new Point(835, 416);
+            panel9.Margin = new Padding(3, 4, 3, 4);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(299, 296);
+            panel9.TabIndex = 14;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Cambria", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(115, 183);
+            label7.Name = "label7";
+            label7.Size = new Size(61, 28);
+            label7.TabIndex = 11;
+            label7.Text = "Ollie";
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.White;
+            button7.Font = new Font("Cambria", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button7.Location = new Point(54, 224);
+            button7.Margin = new Padding(3, 4, 3, 4);
+            button7.Name = "button7";
+            button7.Size = new Size(203, 41);
+            button7.TabIndex = 8;
+            button7.Text = "Select";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.White;
+            pictureBox7.BackgroundImage = (Image)resources.GetObject("pictureBox7.BackgroundImage");
+            pictureBox7.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(35, 32);
+            pictureBox7.Margin = new Padding(3, 4, 3, 4);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(231, 147);
+            pictureBox7.TabIndex = 0;
+            pictureBox7.TabStop = false;
+            // 
+            // panel10
+            // 
+            panel10.BackColor = Color.Silver;
+            panel10.Controls.Add(label8);
+            panel10.Controls.Add(button8);
+            panel10.Controls.Add(pictureBox8);
+            panel10.Location = new Point(448, 416);
+            panel10.Margin = new Padding(3, 4, 3, 4);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(299, 296);
+            panel10.TabIndex = 13;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Cambria", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(99, 183);
+            label8.Name = "label8";
+            label8.Size = new Size(84, 28);
+            label8.TabIndex = 11;
+            label8.Text = "Gregor";
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.White;
+            button8.Font = new Font("Cambria", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button8.Location = new Point(54, 224);
+            button8.Margin = new Padding(3, 4, 3, 4);
+            button8.Name = "button8";
+            button8.Size = new Size(203, 41);
+            button8.TabIndex = 8;
+            button8.Text = "Select";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = Color.White;
+            pictureBox8.BackgroundImage = (Image)resources.GetObject("pictureBox8.BackgroundImage");
+            pictureBox8.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(35, 32);
+            pictureBox8.Margin = new Padding(3, 4, 3, 4);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(231, 147);
+            pictureBox8.TabIndex = 0;
+            pictureBox8.TabStop = false;
+            // 
+            // panel11
+            // 
+            panel11.BackColor = Color.Silver;
+            panel11.Controls.Add(label9);
+            panel11.Controls.Add(button9);
+            panel11.Controls.Add(pictureBox9);
+            panel11.Location = new Point(58, 416);
+            panel11.Margin = new Padding(3, 4, 3, 4);
+            panel11.Name = "panel11";
+            panel11.Size = new Size(299, 296);
+            panel11.TabIndex = 12;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Cambria", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(109, 183);
+            label9.Name = "label9";
+            label9.Size = new Size(89, 28);
+            label9.TabIndex = 11;
+            label9.Text = "Bastian";
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.White;
+            button9.Font = new Font("Cambria", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button9.Location = new Point(54, 224);
+            button9.Margin = new Padding(3, 4, 3, 4);
+            button9.Name = "button9";
+            button9.Size = new Size(203, 41);
+            button9.TabIndex = 8;
+            button9.Text = "Select";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button9_Click;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.BackColor = Color.White;
+            pictureBox9.BackgroundImage = (Image)resources.GetObject("pictureBox9.BackgroundImage");
+            pictureBox9.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox9.Image = (Image)resources.GetObject("pictureBox9.Image");
+            pictureBox9.Location = new Point(35, 32);
+            pictureBox9.Margin = new Padding(3, 4, 3, 4);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(231, 147);
+            pictureBox9.TabIndex = 0;
+            pictureBox9.TabStop = false;
+            // 
+            // panel12
+            // 
+            panel12.BackColor = Color.Silver;
+            panel12.Controls.Add(label10);
+            panel12.Controls.Add(button10);
+            panel12.Controls.Add(pictureBox10);
+            panel12.Location = new Point(835, 76);
+            panel12.Margin = new Padding(3, 4, 3, 4);
+            panel12.Name = "panel12";
+            panel12.Size = new Size(299, 296);
+            panel12.TabIndex = 10;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Cambria", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(115, 183);
+            label10.Name = "label10";
+            label10.Size = new Size(70, 28);
+            label10.TabIndex = 11;
+            label10.Text = "Clyde";
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.White;
+            button10.Font = new Font("Cambria", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button10.Location = new Point(54, 217);
+            button10.Margin = new Padding(3, 4, 3, 4);
+            button10.Name = "button10";
+            button10.Size = new Size(203, 48);
+            button10.TabIndex = 8;
+            button10.Text = "Select";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.BackColor = Color.White;
+            pictureBox10.BackgroundImage = (Image)resources.GetObject("pictureBox10.BackgroundImage");
+            pictureBox10.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox10.Image = (Image)resources.GetObject("pictureBox10.Image");
+            pictureBox10.Location = new Point(35, 32);
+            pictureBox10.Margin = new Padding(3, 4, 3, 4);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(231, 147);
+            pictureBox10.TabIndex = 0;
+            pictureBox10.TabStop = false;
+            // 
+            // panel13
+            // 
+            panel13.BackColor = Color.Silver;
+            panel13.Controls.Add(pictureBox6);
+            panel13.Controls.Add(label11);
+            panel13.Controls.Add(button11);
+            panel13.Location = new Point(448, 76);
+            panel13.Margin = new Padding(3, 4, 3, 4);
+            panel13.Name = "panel13";
+            panel13.Size = new Size(299, 296);
+            panel13.TabIndex = 9;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.White;
+            pictureBox6.BackgroundImage = (Image)resources.GetObject("pictureBox6.BackgroundImage");
+            pictureBox6.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(30, 32);
+            pictureBox6.Margin = new Padding(3, 4, 3, 4);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(231, 147);
+            pictureBox6.TabIndex = 11;
+            pictureBox6.TabStop = false;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Cambria", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.Location = new Point(99, 183);
+            label11.Name = "label11";
+            label11.Size = new Size(100, 28);
+            label11.TabIndex = 10;
+            label11.Text = "Madison";
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.White;
+            button11.Font = new Font("Cambria", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button11.Location = new Point(35, 217);
+            button11.Margin = new Padding(3, 4, 3, 4);
+            button11.Name = "button11";
+            button11.Size = new Size(231, 48);
+            button11.TabIndex = 8;
+            button11.Text = "Select";
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += button11_Click;
+            // 
+            // panel14
+            // 
+            panel14.BackColor = Color.Silver;
+            panel14.Controls.Add(label12);
+            panel14.Controls.Add(button12);
+            panel14.Controls.Add(pictureBox12);
+            panel14.Location = new Point(58, 76);
+            panel14.Margin = new Padding(3, 4, 3, 4);
+            panel14.Name = "panel14";
+            panel14.Size = new Size(299, 296);
+            panel14.TabIndex = 7;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Cambria", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label12.Location = new Point(120, 184);
+            label12.Name = "label12";
+            label12.Size = new Size(55, 28);
+            label12.TabIndex = 9;
+            label12.Text = "Wes";
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.White;
+            button12.Font = new Font("Cambria", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button12.Location = new Point(35, 217);
+            button12.Margin = new Padding(3, 4, 3, 4);
+            button12.Name = "button12";
+            button12.Size = new Size(231, 49);
+            button12.TabIndex = 8;
+            button12.Text = "Select";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += button12_Click;
+            // 
+            // pictureBox12
+            // 
+            pictureBox12.BackColor = Color.White;
+            pictureBox12.BackgroundImage = (Image)resources.GetObject("pictureBox12.BackgroundImage");
+            pictureBox12.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox12.Image = (Image)resources.GetObject("pictureBox12.Image");
+            pictureBox12.Location = new Point(35, 32);
+            pictureBox12.Margin = new Padding(3, 4, 3, 4);
+            pictureBox12.Name = "pictureBox12";
+            pictureBox12.Size = new Size(231, 147);
+            pictureBox12.TabIndex = 0;
+            pictureBox12.TabStop = false;
+            // 
+            // BrowlineBack
+            // 
+            BrowlineBack.FlatStyle = FlatStyle.Popup;
+            BrowlineBack.Font = new Font("Cambria", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            BrowlineBack.Location = new Point(1198, 16);
+            BrowlineBack.Margin = new Padding(3, 4, 3, 4);
+            BrowlineBack.Name = "BrowlineBack";
+            BrowlineBack.Size = new Size(131, 64);
+            BrowlineBack.TabIndex = 20;
+            BrowlineBack.Text = "<  Back";
+            BrowlineBack.UseVisualStyleBackColor = true;
+            BrowlineBack.Click += BrowlineBack_Click;
+            // 
+            // BrowlineForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1445, 1015);
+            Controls.Add(BrowlineBack);
+            Controls.Add(panel8);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "BrowlineForm";
+            Text = "BrowlineForm";
+            panel8.ResumeLayout(false);
+            panel9.ResumeLayout(false);
+            panel9.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            panel10.ResumeLayout(false);
+            panel10.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            panel11.ResumeLayout(false);
+            panel11.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            panel12.ResumeLayout(false);
+            panel12.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            panel13.ResumeLayout(false);
+            panel13.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            panel14.ResumeLayout(false);
+            panel14.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel8;
+        private Panel panel9;
+        private Label label7;
+        private Button button7;
+        private PictureBox pictureBox7;
+        private Panel panel10;
+        private Label label8;
+        private Button button8;
+        private PictureBox pictureBox8;
+        private Panel panel11;
+        private Label label9;
+        private Button button9;
+        private PictureBox pictureBox9;
+        private Panel panel12;
+        private Label label10;
+        private Button button10;
+        private PictureBox pictureBox10;
+        private Panel panel13;
+        private Label label11;
+        private Button button11;
+        private Panel panel14;
+        private Label label12;
+        private Button button12;
+        private PictureBox pictureBox12;
+        private PictureBox pictureBox6;
+        private Button BrowlineBack;
+    }
+}
