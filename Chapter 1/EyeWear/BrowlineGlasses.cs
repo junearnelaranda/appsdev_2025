ae51199bdeb87862e00843c5a4b1a712c1d7cb97
June Arnel Aranda <142424794+junearnelaranda@users.noreply.github.com>
2025-05-17 17:33:44 +0800

    Add files via upload

---
+++ Chapter 1/EyeWear/BrowlineGlasses.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EyeWear
+{
+
+    public partial class BrowlineGlasses : Form
+    {
+        private string _username;
+        public BrowlineGlasses(string username)
+        {
+            InitializeComponent();
+            _username = username;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://sunniesstudios.com/products/carson?variant=42025926557814",
+                UseShellExecute = true
+            });
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://idealvision.ph/product/antepara-austin-ap3028",
+                UseShellExecute = true
+            });
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://idealvision.ph/product/antepara-houston-ap3029",
+                UseShellExecute = true
+            });
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://idealvision.ph/product/ceo-v-cv384",
+                UseShellExecute = true
+            });
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://idealvision.ph/product/ceo-v-cv696",
+                UseShellExecute = true
+            });
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://idealvision.ph/product/police-dynamo1-vpl900k",
+                UseShellExecute = true
+            });
+        }
+
+        private void BrowlineGlasses_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            TriangleForm browlineGlasses = new TriangleForm(_username);
+            this.Hide();
+            browlineGlasses.ShowDialog();
+            this.Close();
+        }
+    }
+}
+
